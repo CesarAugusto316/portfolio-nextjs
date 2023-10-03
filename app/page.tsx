9b354c29a4a4047d6631ff5bbfa34b6b8b24567a
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-10-02 23:03:45 -0500

    ✨ col1 in hero-section updates

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,29 @@ import { FC } from 'react';
 export const Home: FC = () => {
   return (
     <div className={styles.container}>
-      <h1>Hello World</h1>
+      <header className={styles.heroSection}>
+        <div className={styles.col1}>
+
+          <section>
+            <h1>
+              Cesar Rivera <br />
+              Full-Stack <br />
+              Web Developer
+            </h1>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae repellat
+              assumenda harum, et commodi, placeat molestias
+              suscipit doloremque voluptatum distinctio rem ea necessitatibus?
+            </p>
+            <button>Lets work together</button>
+          </section>
+        </div>
+
+        <div className={styles.col2}>
+
+        </div>
+      </header>
     </div>
   );
 };
