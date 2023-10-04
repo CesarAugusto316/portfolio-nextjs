01e903cff677c5a847aef2d4716b51607f3be8c3
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-10-03 20:17:59 -0500

    ✨ logos downloaded

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,48 @@
-import Image from 'next/image';
-import styles from './page.module.scss';
 import { FC } from 'react';
+import Image from 'next/image';
+
+// assets
+import styles from './page.module.scss';
+// import computerImg from '../public/assets/palette.gif';
 
 
 export const Home: FC = () => {
   return (
     <div className={styles.container}>
+
+      <nav className={styles.navBar}>
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>About</li>
+          <li>Contacts</li>
+        </ul>
+      </nav>
+
       <header className={styles.heroSection}>
         <div className={styles.col1}>
 
           <section>
             <h1>
-              Cesar Rivera <br />
+              César Rivera <br />
               <span>Full-Stack</span> <br />
               <span>Web Developer</span>
             </h1>
 
             <p>
-              My name is César Rivera from Ecuador.
               I came into Software Development looking
               for personal growth. But I quickly realized
-              that Software is very challenging and exiting so
+              that Software is very challenging and exiting. So
               I created this space where I can share with the
               world the projects I &apos; am currently working on.
+
+              {/* What truly sets me apart is my critical thinking
+              skills. I have a knack for dissecting complex problems,
+              analyzing them from multiple angles, and arriving at
+              elegant, efficient solutions. This ability not only
+              streamlines my development processes but also ensures
+              that every project I touch delights users with a
+              well crafted design. */}
             </p>
 
             <button className={styles.button}>
@@ -32,10 +52,23 @@ export const Home: FC = () => {
         </div>
 
         <div className={styles.col2}>
+
+          <div className={styles.figures}>
+            <div className={styles.square}></div>
+            <div className={styles.circle}></div>
+            <div className={styles.pentagon}></div>
+          </div>
+
+          {/* <Image className={styles.img} src={computerImg} alt="gif" /> */}
           <div className={styles.dots}></div>
           <div className={styles.blueBlock}></div>
         </div>
       </header>
+
+
+      <section className={styles.projects}>
+        <h2>Latest Work</h2>
+      </section>
     </div>
   );
 };
