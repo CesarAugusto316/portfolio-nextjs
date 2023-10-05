902554286118d02c9802715c5e90d94f1515c2f7
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-10-05 17:36:28 -0500

    ✨ updates

--- app/_components/heroSection/figures/animator/Animator.tsx
+++ app/_components/heroSection/figures/animator/Animator.tsx
@@ -18,10 +18,12 @@ export const Animator: FC<Props> = ({ icons }) => {
   return (
     <div className={styles.container}>
       <Swiper
+        spaceBetween={30}
+        speed={300}
         modules={[Autoplay]}
         loop={true}
         autoplay={{
-          delay: 3_000
+          delay: 2_400
         }}>
         {icons.map((icon, i) => (
           <SwiperSlide key={i} className={styles.swiperSlide}>
