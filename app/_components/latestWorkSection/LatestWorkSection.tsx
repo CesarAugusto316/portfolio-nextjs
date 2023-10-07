c07f5876bfa1f39617e42d9ef597e1194807cc44
cesaraugusto316 <cesaraugusto316@protonmail.com>
2023-10-06 21:56:42 -0500

    ✨ projects cards DONE

--- app/_components/latestWorkSection/LatestWorkSection.tsx
+++ app/_components/latestWorkSection/LatestWorkSection.tsx
@@ -1,7 +1,8 @@
-import Image from 'next/image';
+import { FC } from 'react';
+import { StaticImageData } from 'next/image';
+import { ProjectCard } from './projectCard/ProjectCard';
 
 // assets
-import { FC } from 'react';
 import larnUImg1 from '@/assets/projects/larnu_desktop_01.png';
 import larnUImg2 from '@/assets/projects/larnu_desktop_02.png';
 import larnUImg3 from '@/assets/projects/larnu_desktop_03.png';
@@ -13,28 +14,50 @@ import quizifyImg3 from '@/assets/projects/Screenshot (54).png';
 import quizifyImg4 from '@/assets/projects/Screenshot (55).png';
 import quizifyImg5 from '@/assets/projects/Screenshot (56).png';
 import quizifyImg6 from '@/assets/projects/Screenshot (57).png';
+import ecomerceImg1 from '@/assets/projects/Screenshot (65).png';
+import ecomerceImg2 from '@/assets/projects/Screenshot (66).png';
+import ecomerceImg3 from '@/assets/projects/Screenshot (67).png';
+import ecomerceImg4 from '@/assets/projects/Screenshot (68).png';
+import ecomerceImg5 from '@/assets/projects/Screenshot (69).png';
 import styles from './latestWorkSection.module.scss';
 
 
+export type Project = {
+  images: StaticImageData[],
+  title: string,
+  description: string,
+  repoUrl: string,
+  liveUrl: string,
+  id: number
+}
 
-const PROJECTS = [
+const PROJECTS: Project[] = [
   {
     images: [larnUImg1, larnUImg2, larnUImg3, larnUImg4, larnUImg5],
-    title: 'LarnU Web App',
-    description: 'Bla Bla Bla Bla Bla Bla',
-    repoUrl: '',
-    liveUrl: '',
+    title: 'LarnU',
+    description: 'Our unique approach seamlessly combines data, design, and technology to empower individuals to master languages effectively by simulating real-life conversations',
+    repoUrl: 'https://gitlab.com/larnu-group/larnu-web-app',
+    liveUrl: 'https://dev.larnu.app/',
     id: 1
   },
   {
     images: [quizifyImg1, quizifyImg2, quizifyImg3, quizifyImg4, quizifyImg5, quizifyImg6],
-    title: 'Quizify Web App',
-    description: 'Bla Bla Bla Bla Bla Bla',
-    repoUrl: '',
-    liveUrl: '',
+    title: 'Quizify',
+    description: 'Quizify revolutionizes the learning experience with its proven methodology enhanced by gamification, allowing you to master any subject with ease.',
+    repoUrl: 'https://gitlab.com/quizify/quiz-gpt/web',
+    liveUrl: 'https://quizify.com/',
     id: 2
   },
+  {
+    images: [ecomerceImg1, ecomerceImg2, ecomerceImg3, ecomerceImg4, ecomerceImg5],
+    title: 'E-commerce App',
+    description: 'With Modern design, animations, using a CMS, all advanced cart functionalities, and most importantly the complete integration with Stripe',
+    repoUrl: 'https://github.com/CesarAugusto316/ecommerce-next-sanity',
+    liveUrl: 'https://ecommerce-next-sanity-sigma.vercel.app/',
+    id: 3
+  },
 ];
+
 
 export const LatestWorkSection: FC = () => {
   return (
@@ -45,17 +68,7 @@ export const LatestWorkSection: FC = () => {
 
         <div className={styles.projectsContainer}>
           {PROJECTS.map(project => (
-            <div
-              className={styles.project}
-              key={project.id}
-            >
-              <Image className={styles.img} src={project.images[0]} alt="icon" />
-
-              <div className={styles.body}>
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-              </div>
-            </div>
+            <ProjectCard key={project.id} project={project} />
           ))}
         </div>
       </div>
---
+++ app/_components/latestWorkSection/projectCard/ProjectCard.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { FC } from 'react';
+import Image from 'next/image';
+import { Project } from '../LatestWorkSection';
+
+// assets
+import styles from './projectCard.module.scss';
+import githubIcon from '@/assets/icons/github.svg';
+import rocketIcon from '@/assets/icons/rocket.svg';
+
+
+interface Props {
+  project: Project
+}
+
+export const ProjectCard: FC<Props> = ({ project }) => {
+  return (
+    <div
+      className={styles.project}
+    >
+      <Image className={styles.img} src={project.images[0]} alt="icon" />
+
+      <div className={styles.body}>
+        <h3>{project.title}</h3>
+        <p>{project.description}</p>
+
+        <div className={styles.buttons}>
+          <button>
+            <span>Code</span>
+            <Image height={26} className={styles.icon} src={githubIcon} alt="github-icon" />
+          </button>
+          <button>
+            <span>Deploy</span>
+            <Image height={26} className={styles.icon} src={rocketIcon} alt="github-icon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
